6ed62df719c65c612c2824de2e8569f2460484b0
MR. NEERAJ SINGH <139963133+neerajsingh1802@users.noreply.github.com>
2023-11-04 19:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+//1.  WAP to find whether a given number is odd or even.
+
+// let num=prompt("enter the number");
+
+
+// if(num%2==1){
+//     alert("it is ODD number");
+// }
+// else{
+//     alert("it is even number");
+// }
+
+// 2. Given a no.N, find whether its 2nd last bit from RHS is 1 or 0.
+
+// let num=prompt("enter the number");
+
+// if(num&2){
+//    alert("it is ONE");
+// }
+// else{
+//     alert("it is ZERO");
+// }
+
+// 3.  Given a no.N,find whether its 8th last bit from RHS is 1 or 0.
+
+// let num=prompt("enter the number");
+
+// if(num&(1<<7)){
+//    alert("it is ONE");
+// }
+// else{
+//     alert("it is ZERO");
+// }
+
+// 4.  Given a no.N,find whether its Kth last bit from RHS is 1 or 0.
+
+
+// let num=prompt("enter the number");
+
+// if(num&(1<<(k-1))){
+//    alert("it is ONE");
+// }
+// else{
+//     alert("it is ZERO");
+// }
+
+
+// 5. WAP to find the largest among 3 distinct numbers
+
+let num=prompt("enter the 1st number");
+let num1=prompt("enter the 2nd number");
+let num2=prompt("enter the 3rd number");
+
+if(num>num1 && num>num2){
+alert("num is the largest number");
+}
+else
+    if(num1>num2){
+        alert("num1 is the largest number");
+       }
+       else{
+        alert("num2 is the largest number");
+       }
+
+
+
+
+
+
+
+
+
+
